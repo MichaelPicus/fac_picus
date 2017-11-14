1b48c386a7865aac4319453421b073b5ebaf8106
michael <chenzixing2009@gmail.com>
2017-11-14 12:09:38 -0800

    modified:   blog/views.py db.sqlite3 new file:   ml_models/lasso_model_bilang.pkl

--- blog/views.py
+++ blog/views.py
@@ -140,20 +140,49 @@ def jingbai_ds(request):
     cols = combine.shape[1]
     modified_res = copy.deepcopy(combine)
     for x in range(0, rows):
-        count = 0
-        while combine[x, 0] > 32.9:
-            if count == 3: 
-                break
-            count = count + 1
-            for y in range(1, cols-1):
-                modified_res[x, y] = combine[x, y] * (1 - randint(9700, 10000) / 100000.0)
-            # # modified_res[x, 0] = lgb_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
-            modified_res[x, 0] = combine[x, 0] * (1 - randint(9800, 10000) / 100000.0)
+        # count = 0
+        # while combine[x, 0] > 32.9:
+        #     if count == 3: 
+        #         break
+        #     count = count + 1
+        #     for y in range(1, cols-1):
+        #         modified_res[x, y] = combine[x, y] * (1 - randint(9700, 10000) / 100000.0)
+        #     # modified_res[x, 0] = lgb_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
+        #     modified_res[x, 0] = combine[x, 0] * (1 - randint(9800, 10000) / 100000.0)
 
-            modified_res[x, cols -1] = combine[x, cols-1] * 0.9994448 *randint(980, 999) / 1000
+        #     modified_res[x, cols -1] = combine[x, cols-1] * 0.9994448 *randint(980, 999) / 1000
+        if combine[x, 0] > 33 :
+            # AirOutTemp
+            modified_res[x, 1] = combine[x, 1]  * 0.98
+            # BasePowderTemp
+            modified_res[x, 2] = combine[x, 2]  * 1.035
+            # AirInTemp_1
+            modified_res[x, 3] = combine[x, 3]  * 0.973
+            # SlurryTemp
+            modified_res[x, 4] = combine[x, 4]  * 0.96
+
+            # TowerTopNegativePressure
+            modified_res[x, 5] = combine[x, 5] * 1.001
+            # AgingTankFlow
+            modified_res[x, 6] = combine[x, 6] * 1.028
+            # SecondInputAirTemp
+            modified_res[x, 7] = combine[x, 7] * 1.05
+            # SlurryPipelineLowerLayerPressure
+            modified_res[x, 8] = combine[x, 8] * 0.99
+            # OutAirMotorFreq
+            modified_res[x, 9] = combine[x, 9] * 1.09
+            # SecondAirMotorFreq
+            modified_res[x, 10] = combine[x, 10] * 0.97
+            # HighPressurePumpFreq
+            modified_res[x, 11] = combine[x, 11] * 1.034
+            # GasFlow
+            modified_res[x, 12] = combine[x, 12] * 0.99
+
+            modified_res[x, 0] = lasso_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
 
     final_com = np.column_stack((combine, modified_res))
-    return render(request, 'blog/jingbai_ds.html', {'train_pred': train_pred, 'final_com': final_com})
+ 
+    return render(request, 'blog/jingbai_ds.html', {'final_com': final_com})
 
 
 
@@ -171,24 +200,69 @@ def tbo_ds(request):
     # need to delete the origin upload file
     os.remove(os.path.join(BASE_DIR, 'media/documents/tbo_ready.csv'))
 
+    # rows = combine.shape[0]
+    # cols = combine.shape[1]
+    # modified_res = copy.deepcopy(combine)
+    # for x in range(0, rows):
+    #     count = 0
+    #     while combine[x, 0] > 32.9:
+    #         if count == 3: 
+    #             break
+    #         count = count + 1
+    #         for y in range(1, cols-1):
+    #             modified_res[x, y] = combine[x, y] * (1 - randint(9700, 10000) / 100000.0)
+    #         # # modified_res[x, 0] = lgb_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
+    #         modified_res[x, 0] = combine[x, 0] * (1 - randint(9800, 10000) / 100000.0)
+
+    #         modified_res[x, cols -1] = combine[x, cols-1] * 0.9994448 *randint(980, 999) / 1000
+
+    # final_com = np.column_stack((combine, modified_res))
     rows = combine.shape[0]
     cols = combine.shape[1]
     modified_res = copy.deepcopy(combine)
     for x in range(0, rows):
-        count = 0
-        while combine[x, 0] > 32.9:
-            if count == 3: 
-                break
-            count = count + 1
-            for y in range(1, cols-1):
-                modified_res[x, y] = combine[x, y] * (1 - randint(9700, 10000) / 100000.0)
-            # # modified_res[x, 0] = lgb_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
-            modified_res[x, 0] = combine[x, 0] * (1 - randint(9800, 10000) / 100000.0)
+        # count = 0
+        # while combine[x, 0] > 32.9:
+        #     if count == 3: 
+        #         break
+        #     count = count + 1
+        #     for y in range(1, cols-1):
+        #         modified_res[x, y] = combine[x, y] * (1 - randint(9700, 10000) / 100000.0)
+        #     # modified_res[x, 0] = lgb_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
+        #     modified_res[x, 0] = combine[x, 0] * (1 - randint(9800, 10000) / 100000.0)
 
-            modified_res[x, cols -1] = combine[x, cols-1] * 0.9994448 *randint(980, 999) / 1000
+        #     modified_res[x, cols -1] = combine[x, cols-1] * 0.9994448 *randint(980, 999) / 1000
+        if combine[x, 0] > 33 :
+            # AirOutTemp
+            modified_res[x, 1] = combine[x, 1]  * 0.98
+            # BasePowderTemp
+            modified_res[x, 2] = combine[x, 2]  * 1.035
+            # AirInTemp_1
+            modified_res[x, 3] = combine[x, 3]  * 0.973
+            # SlurryTemp
+            modified_res[x, 4] = combine[x, 4]  * 0.96
+
+            # TowerTopNegativePressure
+            modified_res[x, 5] = combine[x, 5] * 1.001
+            # AgingTankFlow
+            modified_res[x, 6] = combine[x, 6] * 1.028
+            # SecondInputAirTemp
+            modified_res[x, 7] = combine[x, 7] * 1.05
+            # SlurryPipelineLowerLayerPressure
+            modified_res[x, 8] = combine[x, 8] * 0.99
+            # OutAirMotorFreq
+            modified_res[x, 9] = combine[x, 9] * 1.09
+            # SecondAirMotorFreq
+            modified_res[x, 10] = combine[x, 10] * 0.97
+            # HighPressurePumpFreq
+            modified_res[x, 11] = combine[x, 11] * 1.034
+            # GasFlow
+            modified_res[x, 12] = combine[x, 12] * 0.99
+
+            modified_res[x, 0] = lasso_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
 
     final_com = np.column_stack((combine, modified_res))
-    return render(request, 'blog/tbo_ds.html', {'train_pred': train_pred, 'final_com': final_com})
+    return render(request, 'blog/tbo_ds.html', {'final_com': final_com})
 
 
 def bilang_ds(request):
@@ -209,20 +283,48 @@ def bilang_ds(request):
     cols = combine.shape[1]
     modified_res = copy.deepcopy(combine)
     for x in range(0, rows):
-        count = 0
-        while combine[x, 0] > 32.9:
-            if count == 3: 
-                break
-            count = count + 1
-            for y in range(1, cols-1):
-                modified_res[x, y] = combine[x, y] * (1 - randint(9700, 10000) / 100000.0)
-            # # modified_res[x, 0] = lgb_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
-            modified_res[x, 0] = combine[x, 0] * (1 - randint(9800, 10000) / 100000.0)
+        # count = 0
+        # while combine[x, 0] > 32.9:
+        #     if count == 3: 
+        #         break
+        #     count = count + 1
+        #     for y in range(1, cols-1):
+        #         modified_res[x, y] = combine[x, y] * (1 - randint(9700, 10000) / 100000.0)
+        #     # modified_res[x, 0] = lgb_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
+        #     modified_res[x, 0] = combine[x, 0] * (1 - randint(9800, 10000) / 100000.0)
 
-            modified_res[x, cols -1] = combine[x, cols-1] * 0.9994448 *randint(980, 999) / 1000
+        #     modified_res[x, cols -1] = combine[x, cols-1] * 0.9994448 *randint(980, 999) / 1000
+        if combine[x, 0] > 33 :
+            # AirOutTemp
+            modified_res[x, 1] = combine[x, 1]  * 0.98
+            # BasePowderTemp
+            modified_res[x, 2] = combine[x, 2]  * 1.035
+            # AirInTemp_1
+            modified_res[x, 3] = combine[x, 3]  * 0.973
+            # SlurryTemp
+            modified_res[x, 4] = combine[x, 4]  * 0.96
+
+            # TowerTopNegativePressure
+            modified_res[x, 5] = combine[x, 5] * 1.001
+            # AgingTankFlow
+            modified_res[x, 6] = combine[x, 6] * 1.028
+            # SecondInputAirTemp
+            modified_res[x, 7] = combine[x, 7] * 1.05
+            # SlurryPipelineLowerLayerPressure
+            modified_res[x, 8] = combine[x, 8] * 0.99
+            # OutAirMotorFreq
+            modified_res[x, 9] = combine[x, 9] * 1.09
+            # SecondAirMotorFreq
+            modified_res[x, 10] = combine[x, 10] * 0.97
+            # HighPressurePumpFreq
+            modified_res[x, 11] = combine[x, 11] * 1.034
+            # GasFlow
+            modified_res[x, 12] = combine[x, 12] * 0.99
+
+            modified_res[x, 0] = lasso_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
 
     final_com = np.column_stack((combine, modified_res))
-    return render(request, 'blog/bilang_ds.html', {'train_pred': train_pred, 'final_com': final_com})
+    return render(request, 'blog/bilang_ds.html', {'final_com': final_com})
 
 
 
