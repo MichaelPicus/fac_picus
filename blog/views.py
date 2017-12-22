6c761aa64b4adeefbcb840e4005fa6c2f6db4adb
michael <chenzixing2009@gmail.com>
2017-12-22 03:07:23 -0800

    modified:   blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -190,13 +190,13 @@ def jingbai_ds(request):
 
 
 def tbo_ds(request):
-    lasso_model = joblib.load(os.path.join(BASE_DIR, 'ml_models/lasso_model_tbo.pkl'))
+    lasso_model = joblib.load(os.path.join(BASE_DIR, 'ml_models/model_gboost_tbo.pkl'))
     df_ready = pd.read_csv(os.path.join(BASE_DIR, 'media/documents/tbo_ready.csv'))
     train_y = df_ready.M.values
     del df_ready["Unnamed: 0"]
     del df_ready["M"]
     train = df_ready.values
-    train_pred = lasso_model.predict(train)
+    train_pred = np.expm1(lasso_model.predict(train))
 
     combine = np.column_stack((train_pred, train))
     
@@ -237,33 +237,32 @@ def tbo_ds(request):
         #     modified_res[x, cols -1] = combine[x, cols-1] * 0.9994448 *randint(980, 999) / 1000
         if modified_res[x, 0] > 32.5 :
             # AirOutTemp
-            modified_res[x, 1] = combine[x, 1]  * 0.90
+            modified_res[x, 1] = combine[x, 1]  * 0.98
             # BasePowderTemp
-            modified_res[x, 2] = combine[x, 2]  * 1.085
+            modified_res[x, 2] = combine[x, 2]  * 0.99123
             # AirInTemp_1
-            modified_res[x, 3] = combine[x, 3]  * 0.873
+            modified_res[x, 3] = combine[x, 3]  * 1.0456
             # SlurryTemp
-            modified_res[x, 4] = combine[x, 4]  * 0.86
+            modified_res[x, 4] = combine[x, 4]  * 1.0678
 
             # TowerTopNegativePressure
-            modified_res[x, 5] = combine[x, 5] * 1.01
+            modified_res[x, 5] = combine[x, 5] * 0.9877
             # AgingTankFlow
-            modified_res[x, 6] = combine[x, 6] * 1.128
+            modified_res[x, 6] = combine[x, 6] * 1.09678
             # SecondInputAirTemp
-            modified_res[x, 7] = combine[x, 7] * 1.15
+            modified_res[x, 7] = combine[x, 7] * 1.00234
             # SlurryPipelineLowerLayerPressure
-            modified_res[x, 8] = combine[x, 8] * 0.94
+            modified_res[x, 8] = combine[x, 8] * 1.0285
             # OutAirMotorFreq
-            modified_res[x, 9] = combine[x, 9] * 1.13
+            modified_res[x, 9] = combine[x, 9] * 0.9645
             # SecondAirMotorFreq
-            modified_res[x, 10] = combine[x, 10] * 0.92
+            modified_res[x, 10] = combine[x, 10] * 0.98235
             # HighPressurePumpFreq
-            modified_res[x, 11] = combine[x, 11] * 1.074
+            modified_res[x, 11] = combine[x, 11] * 1.054
             # GasFlow
-            modified_res[x, 12] = combine[x, 12] * 0.988
+            modified_res[x, 12] = combine[x, 12] * 0.98667
 
-            modified_res[x, 0] = lasso_model.predict(np.reshape(modified_res[x][1:], (-1, 12)))
-            modified_res[x, 4] = combine[x, 4]  * 1.03568
+            modified_res[x, 0] = np.expm1(lasso_model.predict(np.reshape(modified_res[x][1:], (-1, 12))))
 
     final_com = np.column_stack((combine, modified_res))
     return render(request, 'blog/tbo_ds.html', {'final_com': final_com})
