155c849c5f61f9daa81ed56ec9a3034bcb8ffcdf
michael <chenzixing2009@gmail.com>
2018-11-05 13:19:51 -0800

    blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1585,10 +1585,13 @@ def jingbai_process_v2(data):
             #     modified_res[x, 12] = 500.001
             # else:
             #     modified_res[x, 12] = combine[x, 12] * 0.99857
-            if combine[x, 2] > 109 and density_checking_switch < 635 and density_checking_switch > 600:
+            if combine[x, 2] > 109 and density_checking_switch < 620 and density_checking_switch > 540:
                 if GAS_FLOW >= arr12[x, 12]:
                     modified_res[x, 12] = round(arr12[x, 12], 2)
                     modified_res[x, 0] = np.expm1(model.predict(np.reshape(modified_res[x][1:], (-1, 12))))
+
+                else :
+                    modified_res[x, 12] = round(arr12[x, 12], 2) - 18
                 
 
             # modified_res[x, 0] = np.expm1(model.predict(np.reshape(modified_res[x][1:], (-1, 12))))
