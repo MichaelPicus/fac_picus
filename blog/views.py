040552c21a271e3a333c31b3b95002b4dbf97eea
michael <chenzixing2009@gmail.com>
2018-12-13 12:16:17 -0800

    blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -925,9 +925,9 @@ def value_data_process(request, format=None):
                     'out_air_motor_freq', 'second_air_motor_freq', 'high_pressure_pump_freq', 'gas_flow', 'brand', 'f_m', 'density_checking_switch_2'])
             pred_m = 0
 
-            
+            res = data
             res, pred_m = data_process(data)
-            time.sleep(1)
+
             
             measurement = "value_data"
             host_name = "127.0.0.1"
