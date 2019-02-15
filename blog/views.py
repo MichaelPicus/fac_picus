1d4b770bc42c16c3a715c26d63f7743d7b0d1e57
michael <chenzixing2009@gmail.com>
2019-02-15 02:10:59 -0800

    add views

--- blog/views.py
+++ blog/views.py
@@ -1114,7 +1114,7 @@ def data_process(data):
                 # res, pred_m =jingbai_process_v3(data) # for testing
     elif data['brand'][0] == 4.0:
                 print "processing tbo or others!"
-                res, pred_m= jingbai_process(data)
+                res, pred_m= tbo_process(data)
    
     return res, pred_m
 
