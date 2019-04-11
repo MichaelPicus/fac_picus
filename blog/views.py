2b4655da91410a5c8ad5bf98e3f2ed3488459778
michael <chenzixing2009@gmail.com>
2019-04-11 14:28:00 -0700

    views

--- blog/views.py
+++ blog/views.py
@@ -1442,7 +1442,8 @@ def jingbai_process(data):
                 modified_res[x, 9] = round(combine[x, 9] - 4 * 0.2, 2)
 
             if density_checking_switch > 620 and combine[x, 11] <= 33:
-                modified_res[x, 12] = combine[x, 12] + 12
+                # modified_res[x, 12] = combine[x, 12] + 12
+                pass
 
             # if density_checking_switch > 620 and combine[x, 11] >=34:
                 # modified_res[x, 11] = combine[x, 11] - 1
@@ -1466,9 +1467,10 @@ def jingbai_process(data):
                 modified_res[x, 9] = round(combine[x, 9] + 4 * 0.2, 2)
 
             if combine[x, 2] < float(BPT) and modified_res[x, 10] == -1 and modified_res[x, 9] == -1:
-                modified_res[x, 10] = combine[x, 10] + 1.3
-                modified_res[x, 9] = round(combine[x, 9] + 4 * 0.2, 2)
+                # modified_res[x, 10] = combine[x, 10] + 1.3
+                # modified_res[x, 9] = round(combine[x, 9] + 4 * 0.2, 2)
                 # modified_res[x, 12] = round(combine[x, 12] + 10, 2)
+                pass
 
 
             # if density_checking_switch >=610 and combine[x, 11] >= 34:
@@ -1483,6 +1485,9 @@ def jingbai_process(data):
             if combine[x, 3] < 265:
                 modified_res[x, 10] = combine[x, 10] - 1.5
                 modified_res[x, 9] = round(combine[x, 9] - 4 * 0.2, 2)
+
+            if combine[x, 2] > 110.0 and density_checking_switch < 635:
+                combine[x, 11] = 32
 
             # modified_res[x, 12] = combine[x, 12] - 10
 
