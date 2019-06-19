c982a06cff5c84e25a3b8f8b0b032a255ebb9021
michael <chenzixing2009@gmail.com>
2019-06-19 12:47:28 -0700

    add pipeline pressure

--- blog/views.py
+++ blog/views.py
@@ -1507,7 +1507,7 @@ def jingbai_process(data):
                 indicator = -3
                 cnt = 150
 
-        if abs(float(pre_gasflow) - combine[x, 12]) >= combine[x, 12] * 0.006:
+        if abs(float(pre_gasflow) - combine[x, 12]) >= 3.5:
                 pre_gasflow = combine[x, 12]
                 indicator = -3
                 cnt = 150
