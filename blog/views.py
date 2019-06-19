c93a8671ef16cff923a06546ff7fe5f5bdb37b24
michael <chenzixing2009@gmail.com>
2019-06-19 11:08:36 -0700

    add pipeline pressure

--- blog/views.py
+++ blog/views.py
@@ -1472,10 +1472,10 @@ def jingbai_process(data):
 
             # if combine[x, 2] > 110.0 and density_checking_switch < 635:
             #     modified_res[x, 11] = 32
-            if combine[x, 2] <= 120 and modified_res[x, 9] == -1 and modified_res[x, 10] == -1:
+            if combine[x, 2] <= 115 and modified_res[x, 9] == -1 and modified_res[x, 10] == -1:
                 modified_res[x, 12] = combine[x, 12] + 7
 
-            if combine[x, 2] > 120 and density_checking_switch < 620:
+            if combine[x, 2] > 115 and density_checking_switch < 620:
                 modified_res[x, 12] = combine[x, 12] - 7
 
             if modified_res[x, 10] == -1 and modified_res[x, 11] == -1 and  modified_res[x, 12] == -1 and density_checking_switch < 610 and combine[x, 2] >= 118:
@@ -1488,11 +1488,16 @@ def jingbai_process(data):
             # modified_res[x, 12] = combine[x, 12] - 10
             # jb_tmp = modified_res
         # modified_res = jb_tmp
-        elif combine[x, 2] < 119:
+        elif combine[x, 2] < 115 and combine[x, 8] > 59:
             modified_res[x] = -1
             indicator = 2
             modified_res[x, 10] = combine[x, 10] + 1.3
             modified_res[x, 9] = round(combine[x, 9] + 4 * 0.2, 2)
+            modified_res[x, 12] = combine[x, 12] + 6
+
+        elif combine[x, 2] < 115 and combine[x, 8] < 59:
+            modified_res[x] = -1
+            indicator = 2
             modified_res[x, 12] = combine[x, 12] + 6
         else :
             modified_res[x] = -1
