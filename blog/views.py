2a05af5089a5a02e8db9039f7a93d8df169ad120
michael <chenzixing2009@gmail.com>
2019-06-25 14:40:24 -0700

    modify cnt

--- blog/views.py
+++ blog/views.py
@@ -1511,8 +1511,8 @@ def jingbai_process(data):
         if abs(float(pre_gasflow) - combine[x, 12]) >= 4.0:
                 pre_gasflow = combine[x, 12]
                 indicator = -3
-                if cnt == 0:
-                    cnt = 150
+                # if cnt == 0:
+                # cnt = 150
 
         if abs(float(pre_second) - combine[x, 10]) >= combine[x, 10] * 0.01:
                 pre_second = combine[x, 10]
